ec92ccafcb28f49ce70967da9deee8d8cfde8f9d
Gustaw Hagen Juul Jakobsen <77386033+mogenz@users.noreply.github.com>
2024-06-10 14:52:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,14 @@
-pyperclip
+import subprocess
+import sys
+
+def install_dependencies(requirements_file):
+    with open(requirements_file, 'r') as file:
+        requirements = file.read().splitlines()
+
+    if sys.platform.startswith('linux') or sys.platform.startswith('darwin'):
+        subprocess.run(['pip3', 'install', *requirements], check=True)
+    elif sys.platform.startswith('win'):
+        subprocess.run(['pip', 'install', *requirements], check=True)
+
+requirements_file = './requirements.txt'
+install_dependencies(requirements_file)
